5cac438712c4daea0519821a2c6d7350ab23f54d
mqashoo77 <mohammadqashoo77@gmail.com>
2022-10-26 06:01:36 +0300

    modify Login Page

--- pos-app/src/pages/LoginPage.jsx
+++ pos-app/src/pages/LoginPage.jsx
@@ -1,13 +1,68 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import '../assets/LoginPage.css';
 
-class Login extends Component {
-    state = {  } 
-    render() { 
-        return (
+import {Navigate} from 'react-router-dom'
 
-           <div>Login page </div>
-        );
-    }
-}
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      islogged: false,
+      loginParams: {
+        user_id: "",
+        user_password: ""
+      }
+    };
+  }
+  handleFormChange = event => {
+    let loginParamsNew = { ...this.state.loginParams };
+    let val = event.target.value;
+    loginParamsNew[event.target.name] = val;
+    this.setState({
+      loginParams: loginParamsNew
+    });
+  };
  
-export default Login;
+  login = event => {
+    let user_id = this.state.loginParams.user_id;
+    let user_password = this.state.loginParams.user_password;
+    if (user_id === "admin" && user_password === "123") {
+      localStorage.setItem("token", "T");
+      this.setState({
+        islogged: true
+      });
+    }
+    event.preventDefault();
+  };
+  render() {
+    if (localStorage.getItem("token")) {
+      return <Navigate to="/" /> ;
+    }
+    return (
+      <div className="container">
+        <form onSubmit={this.login} className="form-signin">
+          <h1>Please sign in</h1>
+          <div className="row">
+            <div className="col">
+              <input
+                type="text"
+                name="user_id"
+                onChange={this.handleFormChange}
+                placeholder="Enter Username"
+              />
+              <input
+                type="password"
+                name="user_password"
+                onChange={this.handleFormChange}
+                placeholder="Enter Password"
+              />
+              <input type="submit" value="Login" />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default LoginPage;
