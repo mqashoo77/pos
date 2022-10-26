e5dd3f4c53a39971a68aa6273593693e77252b20
mqashoo77 <mohammadqashoo77@gmail.com>
2022-10-26 06:01:00 +0300

    modify Home Page

--- pos-app/src/pages/HomePage.jsx
+++ pos-app/src/pages/HomePage.jsx
@@ -1,20 +1,46 @@
-import React, { Component } from 'react'
-import Layout from '../components/Layout';
-import Navbar from '../components/Navbar';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 //components
+import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
 
+///// Pages
+import PosPage from './PosPage';
+import Categories from './CategoriesPage';
+import Products from './ProductsPage';
 
 class Home extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <React.Fragment>
-                <Navbar></Navbar>
-                <Layout></Layout>
-            </React.Fragment>
-        );
+  constructor(props) {
+    super(props);
+    this.state = {
+      islogout: false,
+    };
+  }
+  signOut = () => {
+    localStorage.removeItem("token");
+    this.setState({
+      islogout: true,
+    });
+  };
+
+  render() {
+    if (this.state.islogout) {
+      return <Navigate to="/login" />;
     }
+    return (
+      <React.Fragment>
+        <Navbar></Navbar>
+        <Layout signOut={this.signOut}></Layout>
+        <main>
+            <Categories/>
+           
+        </main>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Home;
