6d9c51baeb558323562bd3e8fe1d719ab1beb0bb
mqashoo77 <mohammadqashoo77@gmail.com>
2022-11-01 16:59:49 +0200

    add a product description popup

--- pos-app/src/components/productPageModals.jsx
+++ pos-app/src/components/productPageModals.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Modal } from 'antd';
 
 import ProductForm from "./modals/productForm.jsx"
-import Categories from './../pages/CategoriesPage';
+import ProductDescription from "./modals/productDescription"
 
 function ProductPageModals(
   {
+    products,
     productFormPopModel,
     setProductFormPopModal,
     deleteProductConfirmPopModel,
@@ -14,10 +15,12 @@ function ProductPageModals(
     setProductDescriptionPopModal,
     productId,
     setProductId,
-    categories
+    categories,
+  
   }
 ) {
   return (
+    <div>
     <div>
       {
       productFormPopModel && (
@@ -43,7 +46,30 @@ function ProductPageModals(
      
     }
     </div>
+    <div>
+    {
+    productDescriptionPopModel && (
+      <Modal
+      title="Product Description"
+            open={productDescriptionPopModel}
+      onCancel={() => {
+        setProductDescriptionPopModal(false);
+        setProductId(false);
+      }}
+      footer={null}
+    >
+      <ProductDescription
+        products={products}
+        productId={productId}
+      >
+     
+      </ProductDescription>
+    </Modal>
+    )
    
+  }
+  </div>
+   </div>
   )
 }
 
