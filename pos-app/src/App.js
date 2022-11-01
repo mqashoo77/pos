cd5d3b3318bf93d1228085dd357f8f6fc502847e
mqashoo77 <mohammadqashoo77@gmail.com>
2022-11-01 20:03:26 +0200

    import a fontawesome library to use it some feture in our app

--- pos-app/src/App.js
+++ pos-app/src/App.js
@@ -1,29 +1,30 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 ///// CSS
-import './App.css'
-///// Pages 
-import Login from './pages/LoginPage';
-import Home from './pages/HomePage';
-import PageNotFound from './pages/404Page';
-
+import "./App.css";
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+///// Pages
+import Login from "./pages/LoginPage";
+import Home from "./pages/HomePage";
+import PageNotFound from "./pages/404Page";
+import PosPage from "./pages/PosPage";
+import CategoriesPage from "./pages/CategoriesPage";
+import ProductsPage from "./pages/ProductsPage";
 
 //components
 
-function App () {
-    return (
+function App() {
+  return (
     <BrowserRouter>
-        <React.Fragment>
-              <Routes>
-                <Route path="/login" element={<Login/>} />
-                <Route path="/" element={<Home/>} />
-                <Route path="/404" element={<PageNotFound />} />
-              </Routes>
-          </React.Fragment>
+      <React.Fragment>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/404" element={<PageNotFound />} />
+        </Routes>
+      </React.Fragment>
     </BrowserRouter>
-    
-    
-    );
+  );
 }
- 
-export default App
+
+export default App;
