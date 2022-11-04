8b0dcb5172b36ccaa7cc964891cfb0b7c12e2e02
mqashoo77 <mohammadqashoo77@gmail.com>
2022-11-04 09:00:09 +0200

    category item component for POS page

---
+++ pos-app/src/components/categoryItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function CategoryItem({ categoryName, selectCategoty, setSelectCategoty,choseCategory}) {
+  const handle = (e) => {
+    setSelectCategoty(categoryName)
+    choseCategory()
+  };
+
+  return (
+    <div className="category-wrapper" onClick={handle}>
+      <div className="category-name">
+        <h1>{categoryName}</h1>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryItem;
