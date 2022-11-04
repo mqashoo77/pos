dafe206bd01c54fb574733a2cd3ef443cc69b308
mqashoo77 <mohammadqashoo77@gmail.com>
2022-11-04 09:32:53 +0200

    add an order list component to maneg the selected item

---
+++ pos-app/src/components/orderList.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useEffect } from 'react';
+
+function OrderList({order,setOrder}) {
+
+    function handleUpdateOrder(item){
+      setOrder([...order]);
+    }
+    
+
+  return (
+    <div>
+        {order.map(item => (
+        <li key={item.productName}>
+            <div className="order-item">
+            <p className="order-name">{item.productName}</p>
+            <p className="order-price">{item.productPrice} $</p>
+            <div className="order-control">
+                {item.productCount > 1 ? (<button onClick={()=> {handleUpdateOrder(item);item.productCount-=1}}>
+                <i className="fa-solid fa-minus"></i>
+                </button>):(<button onClick={()=> {handleDeleteItem(item)}}>
+                <i class="fa-solid fa-trash"></i>
+                </button>)}
+                <span>{item.productCount}</span>
+                <button  onClick={()=> {handleUpdateOrder(item);item.productCount+=1}}>
+                <i className="fa-solid fa-plus"></i>
+                </button>
+            </div>
+            </div>
+      </li>
+      ))}
+    </div>
+  )
+  
+}
+
+export default OrderList
