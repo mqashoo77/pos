0aea6c662fc032b3064450adbe7446a46b456e8f
mqashoo77 <mohammadqashoo77@gmail.com>
2022-11-06 14:58:20 +0200

    add orderBill function

--- pos-app/src/components/orderBill.jsx
+++ pos-app/src/components/orderBill.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 
-function OrderBill() {
-  return (
+import { useState,useEffect } from "react";
+import BillingCalculation from "../utils/billingCalculation";
+
+
+function OrderBill({order}) {
+    const [subtotal, setSubtotal] = useState(0)
+    const [tax, setTax] = useState(0)
+    const [total, setTotal] = useState(0)
+
+    useEffect(() => {
+        const a = BillingCalculation(order)
+        setSubtotal(a[0])
+        setTax(a[1])
+        setTotal(a[2])
+      }, [order]);
+  
+    return (
     <div className="order-bill">
       <div className="sub-total">
         <h1>Subtotal</h1>
-        <span>85$</span>
+        <span>{subtotal} $</span>
       </div>
       <div className="tax">
         <h1>Tax</h1>
-        <span>5$</span>
+        <span>{tax} $</span>
       </div>
       <div className="total">
         <div className="total-info">
           <h1>Total</h1>
-          <span>90$</span>
+          <span>{total} $</span>
         </div>
       </div>
     </div>
