295c7e81b33cc5b7698643c2eefc4f0ec46e6f4b
mqashoo77 <mohammadqashoo77@gmail.com>
2022-11-06 12:56:56 +0200

    format

--- pos-app/src/utils/categories.js
+++ pos-app/src/utils/categories.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+
 const getCategories = async () => {
   const { data } = await axios.get("http://127.0.0.1:3000/categories/");
-  return data
+  return data;
 };
 
 export default getCategories;
